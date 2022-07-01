515aa677ca086c03b3dab233ed821500d1d6775f
sureshwebdev22@gmail.com <sureshwebdev22@gmail.com>
2022-07-01 15:02:57 +0530

    commit 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
 import { Test1Component } from './components/test1/test1.component';
+import { Test3Component } from './components/test3/test3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    Test1Component
+    Test1Component,
+    Test3Component
   ],
   imports: [
     BrowserModule,
